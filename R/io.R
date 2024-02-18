16966ad67572248bd082f50ee9b4104a387fa034
lericnet <ericprince82@gmail.com>
2024-02-17 17:52:03 -0700

    added documentation to io.R; updated DESCRIPTION

--- R/io.R
+++ R/io.R
@@ -1,3 +1,18 @@
+#' Read EASL Session XML
+#'
+#' This function reads an XML file and extracts EASL session recording details.
+#' It returns a dataframe with session details including event category, action, name, timestamp, and server time.
+#'
+#' @param xml_file_name A string. The name of the XML file to be read.
+#'
+#' @return A dataframe with columns: 'session', 'eventCategory', 'eventAction', 'eventName', 'timestamp', 'serverTimePretty'.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' read_session_xml("session.xml")
+#' }
+#'
 read_session_xml <- function(xml_file_name) {
   session.recording = xml2::as_list(xml2::read_xml(xml_file_name))
   xml_df <- dplyr::bind_cols(
