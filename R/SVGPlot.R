2930b0ed61da24003a42be971fcf957ca0f0b628
lericnet <ericprince82@gmail.com>
2024-04-21 11:51:31 -0600

    update font size in plots; change SVGPlot to plot all barcodes

--- R/SVGPlot.R
+++ R/SVGPlot.R
@@ -1,6 +1,6 @@
 SVGPlot <- function(type = 'barcodes') {
   switch (type,
-    'barcodes' = cowplot::plot_grid(plotlist=lapply(sessions, plotSelectionsHeatmap)),
+    'barcodes' = cowplot::plot_grid(plotlist=lapply(sessions, plotSessionBarcode)),
     'learnability' = plotTrialDurationPerUser(sessions = sessions),
     'sns' = plotNumeracyOverview(sessions = sessions)
   )
