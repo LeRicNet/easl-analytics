0629d9569f2d226a3fa3bd099e60c11919ff2953
lericnet <ericprince82@gmail.com>
2024-04-29 12:53:32 -0600

    update MCABiPlot

--- R/MCABiPlot.R
+++ R/MCABiPlot.R
@@ -28,11 +28,11 @@ MCABiPlot <- function() {
 
   formatted <- rbind(ind_formatted, var_formatted)
   formatted[grepl('sus', rownames(formatted)),]$symbol <- 'x-dot'
-  formatted[grepl('sus', rownames(formatted)),]$color <- rgb(27,158,119,0.8*255, maxColorValue = 255)
+  formatted[grepl('sus', rownames(formatted)),]$color <- rgb(27,158,119,255, maxColorValue = 255)
   formatted[grepl('sns', rownames(formatted)),]$symbol <- 'cross-dot'
-  formatted[grepl('sns', rownames(formatted)),]$color <- rgb(217,95,2,0.8*255, maxColorValue = 255)
+  formatted[grepl('sns', rownames(formatted)),]$color <- rgb(217,95,2,255, maxColorValue = 255)
   formatted[grepl('^ACP', rownames(formatted)),]$symbol <- 'diamond-x'
-  formatted[grepl('^ACP', rownames(formatted)),]$color <- rgb(117,112,179,0.8*255, maxColorValue = 255)
+  formatted[grepl('^ACP', rownames(formatted)),]$color <- rgb(117,112,179,255, maxColorValue = 255)
 
   dups <- duplicated(formatted[,c(1,2)])
   formatted[dups, 1] <- jitter(formatted[dups, 1], factor = 10)
